31f67cdaf99c23b3b6e7445c13ec601411f951b2
Breakinferno <1972952841@qq.com>
2017-02-21 22:41:15 +0800

    兼容性

--- src/js/modules/HJtags.js
+++ src/js/modules/HJtags.js
@@ -18,25 +18,27 @@ HJ-tags对象属性：index,tags,content.
 	
 	//自定义html标签元素
 	
-	var HJtagsProto = Object.create(HTMLDivElement.prototype);
+/*	var HJtagsProto = Object.create(HTMLDivElement.prototype);
 	//class HJtagsElement extends HTMLDivElement{};
 	HJtagsProto.index = 0;
-	//更新tags
-	function updateTags(tagsHandleId){
-			
-	}
+*/
 	Array.prototype.S=String.fromCharCode(2);
 	Array.prototype.in_array=function(e){
-    	var r = new RegExp(this.S + e + this.S);
-   	 	return (r.test(this.S + this.join(this.S) + this.S));
+		if(this.length == 0)
+			return false;
+		else{
+			var r = new RegExp(this.S + e + this.S);
+   	 		return (r.test(this.S + this.join(this.S) + this.S));
+		}
+    	
 	};
 
-	HJtagsProto.getValue = function(HJtagsEle){
+/*	HJtagsProto.getValue = function(HJtagsEle){
 		//return $(HJtagsEle).attr('value').split(',');
 		return HJtagsEle.tags;
-	}
+	}*/
 
-	function addTag(HJtagsEle,callback){
+/*	function addTag(HJtagsEle,callback){
 		if(HJtagsEle.content != '' && !HJtagsEle.tags.in_array(HJtagsEle.content)){
 			//追加标签
 			//var addtag = '<span class="tagSpan" id="' + content + HJtagsProto.index +'><span>' + content + '</span>&nbsp;&nbsp;<a href="#" title="removeTag">x</a></span>'; 
@@ -87,11 +89,11 @@ HJ-tags对象属性：index,tags,content.
 		that.tags = tagList;
 		that.addCallback = new Function("return " + $(that).attr('addCallback'));//eval('('+$(that).attr('addCallback')+')');
 
-/*		$(that).find('a').bind('onclick',function(){
+		$(that).find('a').bind('onclick',function(){
 			return removeTag(this);
 		});*/
 
-		$(that).bind('keypress',function(event){
+/*		$(that).bind('keypress',function(event){
 			if(event.keyCode == '13'){
 				that.content = $(this).find('.tagInput').val();
 				addTag(that,that.addCallback);
@@ -99,16 +101,98 @@ HJ-tags对象属性：index,tags,content.
 				$(this).find('.tagInput').val('');
 			}
 		});
-	};
+	};*/
 
 
 
 
 	//暴露接口。。感觉很 low
-	window.HJtagsElement = document.registerElement('HJ-tags',{
-		prototype : HJtagsProto
-	});
-	//定义构造函数
+	window.HJElementCounter = 0;
 	
-	//exports('HJtags');
+	class HJTagsElement extends HTMLElement{
+
+		constructor(){
+			super();
+			var that = this;
+			HJElementCounter++;
+/*			that.addEventListener('keypress',function(event){
+				if(event.keyCode == '13'){
+					
+					that.content = $(that).find('.tagInput').val();
+
+					addTag(that,that.addCallback);
+					//$(this).find('.tagInput').val('');
+					//
+					$(that).find('.tagInput').val('');
+				}
+			});*/
+		}
+
+		connectedCallback(){
+			var that = this;
+			that.index = HJElementCounter;
+			that.innerHTML = '<div class="tagsHandle tagsHandle' + HJElementCounter + '">' 
+			+(function(){
+				var inHtml = '';
+				//默认分割符为','号
+				//var tagList = this.attr('value').split(',');
+				var tagStr = $(that).attr('value');
+				var tagList = typeof tagStr === 'undefined' ? new Array() : tagStr.split(',');
+
+				if(tagList[0] == ''){
+					tagList = new Array();
+				}
+				that.tags = tagList;
+				that.addCallback = new Function("return " + $(that).attr('addCallback'));//eval('('+$(that).attr('addCallback')+')');
+				for(var i = 0;i<tagList.length;i++){
+					//为每个tag创建html标签
+					inHtml += '<span class="tagSpan" id="' + tagList[i] + HJElementCounter + '"><span>' + tagList[i] 
+					+ '</span>&nbsp;&nbsp;<a href="#" title="removeTag" onclick="return $(\'#' + tagList[i] + HJElementCounter + '\').remove()">x</a></span>'
+				}
+				return inHtml;
+			}()) 
+			+'<div class= "tagInputDiv" id="addTag' + HJElementCounter + '"><input type="text" class="tagInput tagInput' + HJElementCounter
+			+ '" placeholder="add tag"></input></div></div>';
+
+			//console.log("在这停顿");
+			$(that).bind('keypress',function(event){
+				if(event.keyCode == '13'){
+					that.content = $(this).find('.tagInput').val();
+					this.addTag(that,that.addCallback);
+					//清空输入框
+					$(this).find('.tagInput').val('');
+				}
+			});
+
+		}
+
+		addTag(HJtagsEle,callback){
+			if(HJtagsEle.content != '' && (!HJtagsEle.tags.in_array(HJtagsEle.content))){
+				//追加标签
+				//var addtag = '<span class="tagSpan" id="' + content + HJtagsProto.index +'><span>' + content + '</span>&nbsp;&nbsp;<a href="#" title="removeTag">x</a></span>'; 
+				//$(tagsHandleId).prepend(addtag);
+				//$(addtag).insertBefore($(tagsHandleId).parent());
+				$('<span>').addClass('tagSpan').attr('id',HJtagsEle.content+HJtagsEle.index).append(
+					$('<span>').text(HJtagsEle.content).append('&nbsp;&nbsp;'),
+					$('<a>',{
+						href : '#',
+						title : "removeTag",
+						text : 'x'
+					}).click(function(){
+						return $('#'+HJtagsEle.content+HJtagsEle.index).remove();
+					})).insertBefore('#addTag'+HJtagsEle.index);
+				//push content 入 value
+				//var temp = $(HJtagsEle).attr('value');
+				var addvalue = ($(HJtagsEle).attr('value')+ ',' + HJtagsEle.content);
+				$(HJtagsEle).attr('value',addvalue);
+				HJtagsEle.tags.push(HJtagsEle.content);
+				return callback();
+			}
+		}
+
+	}
+	customElements.define('hj-tags',HJTagsElement);
+	/*window.HJtagsElement = document.registerElement('HJ-tags',{
+		prototype : HJtagsProto
+	});*/
 }(window);
